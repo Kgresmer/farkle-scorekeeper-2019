110709039443c21b391a0c215aa310e2edabca1c
kgresmer <kgresmer@gmail.com>
2019-07-02 21:17:49 -0500

    Creating the new contexts and components for each screen.

---
+++ components/scoreboard.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import DisplayRecentPic from "./display-recent-pic";
+
+interface NavStatelessComponent extends React.StatelessComponent {
+    navigationOptions?: Object
+}
+
+const HomeScreen: NavStatelessComponent = ({ navigation }) => {
+
+    return (
+        <View style={styles.container}>
+
+            <View>
+                {/* score input */}
+                <TouchableOpacity style={styles.buttonContainer}
+                                  onPress={() => navigation.navigate('list')}>
+                    <Text style={styles.buttonText}>Ready to Score!</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonContainer}
+                                  onPress={() => navigation.navigate('list')}>
+                    <Text style={styles.buttonText}>Ready to Score!</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: 'blue'
+    },
+    buttonContainer: {
+        backgroundColor: '#3185cd',
+        padding: 8,
+        margin: 8,
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
+
+HomeScreen.navigationOptions = {
+    title: 'Scoreboard',
+    headerRight: <View />
+};
+
+HomeScreen.propTypes = {
+    /*
+    ...
+    */
+};
+
+
+
--- store.js
+++ store.js
@@ -1,70 +1,47 @@
 import React, { useState } from 'react';
 
-export const PicContext = React.createContext('picture');
+export const RosterContext = React.createContext('roster');
+export const ExistingPlayersContext = React.createContext('existingPlayers');
 export const LoadingContext = React.createContext('loading');
-export const BarcodeListContext = React.createContext('barcodes');
-export const SelectedShipToContext = React.createContext('shipTo');
-export const ShipToListContext = React.createContext('shipToList');
 
 const Store = ({children}) => {
 
-    const [barcodeList, setBarcodeList] = useState([]);
-    const [picData, setPicData] = useState({});
+    const [rosterList, setRosterList] = useState([]);
+    const [existingPlayersList, setExistingPlayersList] = useState(existingPlayerList);
     const [loading, setLoading] = useState('false');
-    const [selectedShipTo, setShipTo] = useState({});
-    const [shipToList, setShipToList] = useState(shiptoList);
 
     return (
         <LoadingContext.Provider value={[loading, setLoading]}>
-            <BarcodeListContext.Provider value={[barcodeList, setBarcodeList]}>
-            <PicContext.Provider value={[picData, setPicData]}>
-            <SelectedShipToContext.Provider value={[selectedShipTo, setShipTo]}>
-            <ShipToListContext.Provider value={[shipToList, setShipToList]}>
+            <RosterContext.Provider value={[rosterList, setRosterList]}>
+            <ExistingPlayersContext.Provider value={[existingPlayersList, setExistingPlayersList]}>
                 {children}
-            </ShipToListContext.Provider>
-            </SelectedShipToContext.Provider>
-            </PicContext.Provider>
-            </BarcodeListContext.Provider>
+            </ExistingPlayersContext.Provider>
+            </RosterContext.Provider>
         </LoadingContext.Provider>
     )
 };
 
-const shiptoList = [
+const existingPlayerList = [
   {
     key: '1',
-    shipto: 54556632,
-    acct: 54464432,
-    active: false
+    wins: 3,
+    losses: 2,
+    bestScore: 10100,
+    name: 'Kevin'
   },
   {
     key: '2',
-    shipto: 14516632,
-    acct: 14464432,
-    active: false
+    wins: 1,
+    losses: 4,
+    bestScore: 10500,
+    name: 'Sigrid'
   },
   {
     key: '3',
-    shipto: 24556632,
-    acct: 34464432,
-    active: false
-  },
-  {
-    key: '4',
-    shipto: 61884,
-    acct: 225472,
-    active: false
-  },
-  {
-    key: '5',
-    shipto: 3211144,
-    acct: 1113253,
-    active: false
-  },
-  {
-    key: '6',
-    shipto: 68181,
-    acct: 68291,
-    active: false
+    wins: 5,
+    losses: 3,
+    bestScore: 10600,
+    name: 'Rose'
   }
 ];
 
